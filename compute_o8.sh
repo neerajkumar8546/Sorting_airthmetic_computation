6b3e8105fc40c06b11434be7ccabffbc64721e6e
unknown <neerajkumar8546@gmail.com>
2020-05-19 16:13:06 +0530

    airthmetic equation

---
+++ compute_o8.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -x
+
+declare -A store_result
+counter=0
+
+read -p "Enter a = " a
+read -p "Enter b = " b
+read -p "Enter c = " c
+
+calc1=$((a+((b*c))))
+calc2=$((((a*b))+c))
+calc3=$((c+((a/b))))
+calc4=$((((a%b))+c))
+
+#store results in dictionary
+store_result[0]=$calc1
+store_result[1]=$calc2
+store_result[2]=$calc3
+store_result[3]=$calc4
+
+#store results dictionary to array
+
+store_result_toArray[ counter++ ]="${store_result[0]}"
+store_result_toArray[ counter++ ]="${store_result[1]}"
+store_result_toArray[ counter++ ]="${store_result[2]}"
+store_result_toArray[ counter++ ]="${store_result[3]}"
+
+
+#print dictionary value
+#echo "${store_result[@]}"
+printf "\n"
+#print array value
+echo ${store_result_toArray[@]}
+
+
+# Performing Bubble sort  
+for ((i = 0; i<4; i++)) 
+do
+      
+    for((j = 0; j<4-i-1; j++)) 
+    do
+      
+        if [ ${store_result_toArray[j]} -lt ${store_result_toArray[$((j+1))]} ] 
+        then
+            # swap 
+            temp=${store_result_toArray[j]} 
+            store_result_toArray[$j]=${store_result_toArray[$((j+1))]}   
+            store_result_toArray[$((j+1))]=$temp 
+        fi
+    done
+done
+
+
+printf "\n"
+#print sorted array desending order
+echo "print sorted array descending order : "
+printf "\n"
+echo ${store_result_toArray[@]}
